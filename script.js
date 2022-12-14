d6811ced6abbb9b8a750834d8314fbc757f7cbf9
Priya Prakash <priyprakashpandey@gmail.com>
2022-12-15 01:32:29 +0530

    Snake game complete

---
+++ script.js
@@ -0,0 +1,118 @@
+
+let btn = document.getElementById("btn");
+
+//Board
+let blockSize = 25;
+let row = 25;
+let cols = 25;
+
+let board;
+let context;
+
+//Snake
+let snakeX = blockSize * 5;
+let snakeY = blockSize * 5;
+
+let velocityX = 0;
+let velocityY = 0;
+
+let snakeBody = [];
+
+//Food
+let foodX;
+let foodY;
+
+let gameOver = false;
+
+function placeFood() {
+  foodX = blockSize * Math.floor(Math.random() * row); //Return random number
+  foodY = blockSize * Math.floor(Math.random() * cols); //Return random number
+}
+
+window.onload = function () {
+  board = document.getElementById('board');
+  board.height = row * blockSize;
+  board.width = row * blockSize;
+
+  context = board.getContext('2d');
+  document.addEventListener('keyup', changeDirection);
+
+  placeFood();
+  // update();
+  setInterval(update, 100); //100 milisecond
+};
+
+//Changing Direction
+
+function changeDirection(e) {
+  if (e.code == 'ArrowUp' && velocityY != 1) {
+    velocityX = 0;
+    velocityY = -1;
+  } else if (e.code == 'ArrowDown' && velocityY != -1) {
+    velocityX = 0;
+    velocityY = 1;
+  } else if (e.code == 'ArrowLeft' && velocityX != 1) {
+    velocityX = -1;
+    velocityY = 0;
+  } else if (e.code == 'ArrowRight' && velocityX != -1) {
+    velocityX = 1;
+    velocityY = 0;
+  }
+}
+
+function update() {
+  if (gameOver) {
+    btn.style.visibility = "visible"
+    return;
+  }
+
+  context.fillStyle = 'black';
+  context.fillRect(0, 0, board.width, board.height);
+
+  if (foodX == snakeX && foodY == snakeY) {
+    snakeBody.push([foodX, foodY]);
+    placeFood();
+  }
+
+  context.fillStyle = 'red';
+  context.fillRect(foodX, foodY, blockSize, blockSize);
+
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+    snakeBody[i] = snakeBody[i - 1];
+  }
+
+  if (snakeBody.length) {
+    snakeBody[0] = [snakeX, snakeY];
+  }
+
+  context.fillStyle = 'lime';
+  snakeX += velocityX * blockSize;
+  snakeY += velocityY * blockSize;
+  context.fillRect(snakeX, snakeY, blockSize, blockSize);
+  for (let i = 0; i < snakeBody.length; i++) {
+    context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+  }
+
+  //Game over conditions
+  if (
+    snakeX < 0 ||
+    snakeX > cols * blockSize ||
+    snakeY < 0 ||
+    snakeY > row * blockSize
+  ) {
+    gameOver = true;
+    // alert('Game Over');
+    // location.reload()
+  }
+
+  for (let i = 0; i < snakeBody.length; i++) {
+    if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
+      gameOver = true;
+      // alert('Game Over');
+      // location.reload()
+    }
+  }
+}
+btn.addEventListener("click", ()=>{
+  location.reload()
+})
